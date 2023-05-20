842b7a61be220308c015bc7a4815373833e6132f
Yves Quemener <yves.quemener@gmail.com>
2023-05-20 21:44:21 +0200

    Fixing docker persistence

--- configuration.py
+++ configuration.py
@@ -3,7 +3,7 @@ import openai,os
 # HOMESERVER_URL = "https://matrix-client.matrix.org"
 HOMESERVER_URL = "https://matrix.iv-labs.org:8448"
 # TODO nginx redirect from 80 (and https) to 8448
-DOCKER_ROOT_DIR = "/app/"
+DOCKER_ROOT_DIR = "/app/db/"
 
 if os.path.exists(DOCKER_ROOT_DIR):
     ROOT_DIR = DOCKER_ROOT_DIR
