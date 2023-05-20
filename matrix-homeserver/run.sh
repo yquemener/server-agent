32c18ec1811ac07e542a3792d640ffb983769aeb
Yves Quemener <yves.quemener@gmail.com>
2023-05-20 02:30:39 +0200

    Fixed some parameters for the homeserver

--- matrix-homeserver/run.sh
+++ matrix-homeserver/run.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-docker run -it --rm --name culture-matrix-server culture-matrix-server     \
-    -v /opt/synapse:/data \
-    -e SYNAPSE_SERVER_NAME=matrix.iv-labs.org \
-    -e SYNAPSE_REPORT_STATS=no
+docker run -it --rm --name culture-matrix-server \
+          -v /opt/synapse:/data \
+          -v /etc/letsencrypt/live/matrix.iv-labs.org:/etc/letsencrypt/live/matrix.iv-labs.org \
+          -v /etc/letsencrypt/archive/matrix.iv-labs.org:/etc/letsencrypt/archive/matrix.iv-labs.org \
+          -e SYNAPSE_SERVER_NAME=matrix.iv-labs.org \
+          -e SYNAPSE_REPORT_STATS=no \
+          -p 8008:8008 -p 8448:8448 culture-matrix-server
